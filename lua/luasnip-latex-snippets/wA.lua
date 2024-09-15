bb1a3322282082cfd135bb13bae038323b5efec8
Arjun Bhamra <33864884+abhamra@users.noreply.github.com>
2024-09-15 15:34:58 -0400

    Update wA.lua, add tbb and tii

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -12,6 +12,8 @@ function M.retrieve(not_math)
   return {
     parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
     parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
+    parse_snippet({ trig = "tbb", name = "Bold Text" }, "\textbf{$1} $0"),
+    parse_snippet({ trig = "tii", name = "Italic Text" }, "\textit{$1} $0"),
   }
 end
 
